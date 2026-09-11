8c2c7f17c60c11978a54db845a47fabd351246d3
MWLoppo <bobos7477@gmail.com>
2026-09-11 10:04:40 +0900

    Set Cloudflare build version to CF MWS V 1.0.4

--- src/cf-v571.js
+++ src/cf-v571.js
@@ -1,6 +1,6 @@
 import cf57 from './cf-v57.js';
 
-const BUILD_VERSION = 'CF MWS V 1.0.3';
+const BUILD_VERSION = 'CF MWS V 1.0.4';
 
 async function withBuildVersion(response, request) {
   if (!response) return response;
