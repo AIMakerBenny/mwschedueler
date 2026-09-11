210c8a244787001078b19e1a18887aacd01cf297
MWLoppo <bobos7477@gmail.com>
2026-09-11 15:10:02 +0900

    Bump worker build version to 1.0.7

--- src/cf-v571.js
+++ src/cf-v571.js
@@ -1,6 +1,6 @@
 import cf57 from './cf-v57.js';
 
-const BUILD_VERSION = 'CF MWS V 1.0.6';
+const BUILD_VERSION = 'CF MWS V 1.0.7';
 
 async function withBuildVersion(response, request) {
   if (!response) return response;
