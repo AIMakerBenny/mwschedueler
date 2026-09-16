06c558829a4a42a774a16fcf9fefd3ba6cb83805
MWLoppo <bobos7477@gmail.com>
2026-09-17 05:34:55 +0900

    Bump Majoku Castle sidebar asset after records room removal

--- src/cf-v111-entry.js
+++ src/cf-v111-entry.js
@@ -2,7 +2,7 @@ import authWorker from './cf-v112-authfix.js';
 
 const STEAM_SCRIPT='<script src="/assets/steam-game-v111.js?v=1.1.2" data-mws-steam-v111="1"></script>';
 const CALENDAR_DRAG_FIX='<link rel="stylesheet" href="/assets/calendar-drag-layout-fix.css?v=1.1.0" data-mws-calendar-drag-fix="1">';
-const MAJOKU_SIDEBAR_SCRIPT='<script src="/assets/majoku-sidebar-v111.js?v=1.1.1" data-mws-majoku-sidebar="1"></script>';
+const MAJOKU_SIDEBAR_SCRIPT='<script src="/assets/majoku-sidebar-v111.js?v=1.1.2" data-mws-majoku-sidebar="1"></script>';
 
 function htmlResponse(response,html,extraHeaders={}){
   const headers=new Headers(response.headers);
@@ -31,7 +31,7 @@ export default {
       if(!html.includes('majoku-sidebar-v111.js')){
         html=html.includes('</body>')?html.replace('</body>',`${MAJOKU_SIDEBAR_SCRIPT}\n</body>`):`${html}\n${MAJOKU_SIDEBAR_SCRIPT}`;
       }
-      return htmlResponse(response,html,{'x-mws-majoku-sidebar':'v1.1.1'});
+      return htmlResponse(response,html,{'x-mws-majoku-sidebar':'v1.1.2'});
     }
 
     if (url.pathname !== '/' && url.pathname !== '/index.html') return response;
