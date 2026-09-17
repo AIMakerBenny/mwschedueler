4b4a14e4e6ce0b46f3c3729b47cbb7aabaa4ee81
MWLoppo <bobos7477@gmail.com>
2026-09-17 19:22:38 +0900

    Bump Mawang Scheduler display version to v1.3.0

--- assets/app-version-v120.js
+++ assets/app-version-v120.js
@@ -1,10 +1,10 @@
-/* Mawang Scheduler v1.2.0 - single authoritative version display */
+/* Mawang Scheduler v1.3.0 - single authoritative version display */
 (()=>{
 'use strict';
 if(window.__mwsAppVersionV120)return;
 window.__mwsAppVersionV120=1;
 
-const VERSION='1.2.0';
+const VERSION='1.3.0';
 const LABEL=`Mawang Scheduler v ${VERSION}`;
 const BUILD=`MWS V ${VERSION}`;
 window.MWS_APP_VERSION=VERSION;
